205fe164fb55fbf13a076276093f43771f432043
MiguelTucto <u201521691@upc.edu.pe>
2023-07-13 19:57:44 -0500

    feat: route is in progress

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 import {connectToDB} from "@utils/database";
-
+import User from "@models/user";
 
 
 const handler = NextAuth({
@@ -12,6 +12,13 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        const sessionUser = await  User.findOne({
+            email: session.user.email
+        })
+
+        session.user.id = sessionUser._id.toString();
+
+        return session;
 
     },
     async signIn({ profile }){
@@ -19,9 +26,17 @@ const handler = NextAuth({
             await connectToDB();
 
             // check if a user already exists
-
+            const userExists = await User.findOne({
+                email: profile.email
+            })
             //if not, create a new user
-
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace("", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
             return true;
         } catch (error) {
             console.log(error);
