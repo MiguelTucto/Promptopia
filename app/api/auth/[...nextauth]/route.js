39e525bd934ba56f92d7c9322ec1436d03aa4ee1
MiguelTucto <u201521691@upc.edu.pe>
2023-07-13 19:37:14 -0500

    feat: implementing route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
+import {connectToDB} from "@utils/database";
 
 const handler = NextAuth({
     providers: [
@@ -12,7 +13,18 @@ const handler = NextAuth({
 
     },
     async signIn({ profile }){
+        try {
+            await connectToDB();
 
+            // check if a user already exists
+
+            //if not, create a new user
+            
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
     }
 })
 
